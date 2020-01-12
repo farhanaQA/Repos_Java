830198d7781943176585c1ab9df5f363873b2a8a
Farhana Bushra <farhanabushra@Farhanas-MBP.hsd1.ca.comcast.net>
2020-01-12 13:47:57 -0800

    Edited Convert Temperature

--- src/OOP/Temperature.java
+++ src/OOP/Temperature.java
@@ -9,33 +9,27 @@ public class Temperature {
 
         //Convert temperature celsius to Kelvin
         double celciousToK =temp.Covertkelvin(45);
-        if(celciousToK>0){
-            System.out.println("Convert temperature celcius to kelvin: "+celciousToK+"°");
-        }
+        System.out.println("Convert temperature celcius to kelvin: "+celciousToK+"°");
 
 
         //Convert temperature   Celsius to farenheit
         double celciousToFar=temp.ConvertFarenheit(40);
-        if(celciousToFar> 40){
-            System.out.println("farenheit value is " +celciousToFar+"° "+ " ,its too hot outside: ");
+        System.out.println("Convert temperature  celcius to farenheit:" +celciousToFar+"° ");
 
-        }
 
         //Convert temperature kelvin to celsius
         double kelvinToCel=temp.ConvertCelcius(300);
-        if(kelvinToCel< 280){
-            System.out.println("Convert temperature kelvin To Celcius "+ kelvinToCel +"° ");
-        }
+        System.out.println("Convert temperature kelvin To Celcius "+ kelvinToCel +"° ");
+
 
 
         //Convert temperature kelvin to farenheit
         double kelvinTofar=temp.ConvertFareheit(320);
-        if(kelvinTofar>100)
-        {
-            System.out.println("farenheit value is "+kelvinTofar+"°"+", its too hot outside  : ");
-        }
-    }
-
+        System.out.println("Convert temerature kelvin to farenheit :"+kelvinTofar+"° : ");
 
     }
 
+
+
+
+}
